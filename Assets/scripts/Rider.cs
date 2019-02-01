8bd6d229a78f8b465ea28764b80e302e8b180ef0
S 550 <S 550@JOHN-PC>
2019-02-02 05:32:23 +0800

    physics test

--- Assets/scripts/Rider.cs
+++ Assets/scripts/Rider.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Rider : MonoBehaviour {
 
+	Rigidbody2D rigidbody;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter2D(Collision2D c)
+	{
+		EdgeCollider2D e = c.gameObject.GetComponent<EdgeCollider2D>();
+		Accelerator a = c.gameObject.GetComponent<Accelerator>();
+
+		if (a)
+		{
+			Vector2 v = (e.points[1] - e.points[0]).normalized * a.amount;
+			rigidbody.AddForce(v, ForceMode2D.Force);
+		}
 	}
 }
