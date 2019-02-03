fa3913c8dd155c699410b8cb83adfcb896ea6f52
S 550 <S 550@JOHN-PC>
2019-02-03 11:29:14 +0800

    basic touch pan test

--- Assets/scripts/LineEditor.cs
+++ Assets/scripts/LineEditor.cs
@@ -2,40 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineEditor : MonoBehaviour {
+public class LineEditor : MonoBehaviour
+{
 
-	private static LineEditor _main;
-	public static LineEditor main
+    private static LineEditor _main;
+    public static LineEditor main
+    {
+        get
+        {
+            if (!_main)
+            {
+                _main = GameObject.FindObjectOfType<LineEditor>();
+            }
+
+            return _main;
+        }
+    }
+
+    LineEditMode lineEditMode;
+
+    void Awake()
+    {
+
+        SetEditMode(LineEditMode.DrawNormal);
+
+        var recognizer = new TKPanRecognizer();
+
+        recognizer.gestureRecognizedEvent += (r) =>
+        {
+			OnPressStart(r.touchLocation());
+        };
+
+        // continuous gestures have a complete event so that we know when they are done recognizing
+        recognizer.gestureCompleteEvent += r =>
+        {
+			OnPressEnd(r.touchLocation());
+        };
+
+		TouchKit.addGestureRecognizer(recognizer);
+    }
+
+	public void OnPressStart(Vector2 point)
 	{
-		get{
-			if (!_main)
-			{
-				_main = GameObject.FindObjectOfType<LineEditor>();
-			}
-
-			return _main;
-		}
+		print(point.ToString());
 	}
 
-	LineEditMode lineEditMode;
-
-	void Awake () {
-		
-		SetEditMode(LineEditMode.DrawNormal);
-	}
-	
-	void Update () {
-		
-	}
-
-	public void SetEditMode(LineEditMode e)
+	public void OnPressEnd(Vector2 point)
 	{
-		lineEditMode = e;
-		HUD.main.SetCurrentTool(e);
+		print(point.ToString());
 	}
 
-	public enum LineEditMode
-	{
-		DrawNormal, DrawAccelerator, DrawBounce, DrawBrake, Erase, Camera
-	}
+    void Update()
+    {
+
+    }
+
+    public void SetEditMode(LineEditMode e)
+    {
+        lineEditMode = e;
+        HUD.main.SetCurrentTool(e);
+    }
+
+    public enum LineEditMode
+    {
+        DrawNormal, DrawAccelerator, DrawBounce, DrawBrake, Erase, Camera
+    }
 }
